c3117d71e8df22eef2204bc3bc69f478db93d49d
iosypov <ivan.osypov@redocly.com>
2022-10-18 16:54:28 +0300

    chore: prettier

--- src/hooks/useFeedbackApi.tsx
+++ src/hooks/useFeedbackApi.tsx
@@ -1,12 +1,10 @@
-import {
-  Client, FeedbackController,
-} from 'blips-and-chitz-feedback-api-sdk';
+import { Client, FeedbackController } from 'blips-and-chitz-feedback-api-sdk';
 const client = new Client({
   timeout: 0,
   xRapidAPIKey: '637316bb60msh72029ce58a98a83p19d6d5jsnb9fefc607f61',
   xAPIKEY: 'LgcVsiwdXDt1AYSqtf5fbt4GYhk2jGGW',
   xRapidAPIHost: 'feedback-api5.p.rapidapi.com',
-})
+});
 const publicController = new FeedbackController(client);
 function useFeedbackApi() {
   function postRating(rating: number) {
